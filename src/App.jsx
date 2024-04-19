bc9c7286104c972011331e4f8ab77c98abf8256a
macikb8@yahoo.com <macikb8@yahoo.com>
2024-04-19 17:56:22 +0200

    built the sort functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,167 @@
 import { useState } from "react";
 import "./App.css";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import { CiSearch } from "react-icons/ci";
 
+export default function App() {
+  const [todos, setTodos] = useState([]);
 
- export default function App() {
-  const  [todos, setTodos] = useState([]);
-
-   function handleAdditem(item) {
-    setTodos(((todos) => [...todos, item]))
+  function handleAdditem(item) {
+    setTodos((todos) => [...todos, item]);
   }
 
   function handleDeleteItem(id) {
-    setTodos((todos) => 
-      todos.filter((todo) => 
-      todo.id !== id))
+    setTodos((todos) => todos.filter((todo) => todo.id !== id));
   }
 
   function handleToggleItem(id) {
-    setTodos((todos) => 
-      todos.map((todo) => 
-      todo.id === id ? {...todo, checked: !todo.checked} : todo ))
+    setTodos((todos) =>
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, checked: !todo.checked } : todo
+      )
+    );
   }
 
   return (
     <div className="App">
       <Form onAdditem={handleAdditem} />
-      <TodoList todos={todos} onDeleteItem={handleDeleteItem} onToggleItem={handleToggleItem} />
+      <TodoList
+        todos={todos}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
     </div>
-  )
+  );
 }
 
-function Form({onAdditem}) {
-  const  [todoInput, setTodoInput] = useState("")
+function Form({ onAdditem }) {
+  const [todoInput, setTodoInput] = useState("");
+  
 
   function handleSubmit(e) {
     e.preventDefault();
-    if(!todoInput) return;
-    const newTodo = {id: Date.now(), todo: todoInput, checked: false}
-    onAdditem(newTodo)
-    setTodoInput('');
-   
+    if (!todoInput) return;
+    const newTodo = { id: Date.now(), todo: todoInput, checked: false };
+    onAdditem(newTodo);
+    setTodoInput("");
   }
 
- return (
-  <div className="add-form">
-    <form onSubmit={handleSubmit} style={{ width: '500px', backgroundColor: 'white', padding: '60px 80px', position: 'relative'}}>
-      <h1 className="todo-title">TODO LIST</h1>
-      <input value={todoInput} onChange={(e) => setTodoInput(e.target.value)} type="text" placeholder="Search Note" style={{ width: '100%', padding: '8px 30px 8px 40px', borderRadius: '5px', border: '1px solid #ccc', boxSizing: 'border-box' }}
- />
-      <button><CiSearch className="search-icon" size={'20px'} /></button>
-      <select name="todos">
-        <option value="all">ALL</option>
-        <option value="checked">Checked</option>
-      </select>
+  return (
+    <div className="add-form">
+      <form
+        onSubmit={handleSubmit}
+        style={{
+          width: "500px",
+          backgroundColor: "white",
+          padding: "60px 80px",
+          position: "relative",
+        }}
+      >
+        <div>
+          {" "}
+          <h1 className="todo-title">TODO LIST</h1>
+        </div>
+        <div>
+        <input
+          value={todoInput}
+          onChange={(e) => setTodoInput(e.target.value)}
+          type="text"
+          placeholder="Search Note"
+          style={{
+            width: "100%",
+            padding: "8px 30px 8px 40px",
+            borderRadius: "5px",
+            border: "1px solid #ccc",
+            boxSizing: "border-box",
+          }}
+        />
+        <button>
+          <CiSearch className="search-icon" size={"20px"} />
+        </button>
+        </div>
+       
+       
       </form>
-  </div>
- )
+    </div>
+  );
 }
 
-function TodoList({todos, onDeleteItem, onToggleItem }) {
- return (
-  <div className="todo-list">
-    <ul style={{listStyle: 'none'}}>
-    {todos.map(todo => <Item onDeleteItem={onDeleteItem} onToggleItem={onToggleItem} todo={todo} key={todo.id} />)}
-    </ul>
-  </div>
- )
-}
+function TodoList({ todos, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("all");
 
-function Item({todo, onDeleteItem, onToggleItem}) {
+  let sortedItems;
+
+  if (sortBy === "all") {
+    sortedItems = todos;
+  } else if (sortBy === "title") {
+    sortedItems = todos.sort((a, b) => a.todo.localeCompare(b.todo));
+  } else if (sortBy === "checked") {
+    sortedItems = todos.filter((todo) => todo.checked);
+  }   
 
   return (
+    <div className="todo-list">
+      <ul style={{ listStyle: "none" }}>
+        {sortedItems && sortedItems.map((todo) => (
+          <Item
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            todo={todo}
+            key={todo.id}
+          />
+        ))}
+      </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="all">All</option>
+          <option value="title">Title</option>
+          <option value="checked">Checked</option>
+        </select>
+      </div>
+    </div>
+  );
+}
+
+function Item({ todo, onDeleteItem, onToggleItem }) {
+  return (
     <li>
-      <label >
-      <input style={{backgroundColor: todo.checked ? 'lightblue' : 'transparent' }} type="checkbox" value={todo.checked} onChange={() => {onToggleItem(todo.id)}}  />
-      <span style={todo.checked ? {textDecoration: 'line-through', color: 'lightgray'} : {} }>{todo.todo}</span>
+      <label>
+        <input
+          style={{
+            backgroundColor: todo.checked ? "lightblue" : "transparent",
+          }}
+          type="checkbox"
+          value={todo.checked}
+          onChange={() => {
+            onToggleItem(todo.id);
+          }}
+        />
+        <span
+          style={
+            todo.checked
+              ? { textDecoration: "line-through", color: "lightgray" }
+              : {}
+          }
+        >
+          {todo.todo}
+        </span>
       </label>
       <button onClick={() => onDeleteItem(todo.id)}>X</button>
     </li>
-  )
+  );
 }
 
 TodoList.propTypes = {
-  todos: PropTypes.func.isRequired,
+  todos: PropTypes.any,
   onDeleteItem: PropTypes.func,
-  onToggleItem: PropTypes.func
-}
+  onToggleItem: PropTypes.func,
+};
 Item.propTypes = {
   item: PropTypes.string,
   onDeleteItem: PropTypes.func,
-  todo: PropTypes.string,
-  onToggleItem: PropTypes.func
-}
+  todo: PropTypes.object,
+  onToggleItem: PropTypes.func,
+};
 Form.propTypes = {
-  onAdditem: PropTypes.func
-}
+  onAdditem: PropTypes.func,
+};
